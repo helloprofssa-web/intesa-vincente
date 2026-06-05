9490d75b785a1166120a6c2149a19e199645e964
Maria Luisa <helloprofssa@gmail.com>
2026-06-05 12:01:20 +0200

    Aggiunge argomento combinato reti + intelligenza artificiale

--- src/data/topics.js
+++ src/data/topics.js
@@ -3,11 +3,16 @@ import databaseWords from './database'
 import intelligenzaArtificialeWords from './intelligenza-artificiale'
 import retiSicurezzaWords from './reti-sicurezza'
 
+const retiIntelligenzaArtificialeWords = [
+  ...new Set([...retiSicurezzaWords, ...intelligenzaArtificialeWords]),
+]
+
 const wordsByTopic = {
   algoritmica: algoritmicaWords,
   database: databaseWords,
   'intelligenza artificiale': intelligenzaArtificialeWords,
   'reti e sicurezza': retiSicurezzaWords,
+  'reti + intelligenza artificiale': retiIntelligenzaArtificialeWords,
 }
 
 export default wordsByTopic
